90c91efac62a5f69b32d3a6950f2b914473879e0
coffeefrea <coffeefreaky@hotmail.com>
2020-01-03 17:00:33 +0100

    Add files via upload

---
+++ brain/succbot.py
@@ -0,0 +1,47 @@
+import random
+from discord import Game
+from discord.ext.commands import Bot
+import config
+import pornhub
+
+BOT_PREFIX = "!"
+
+client = Bot(command_prefix=BOT_PREFIX)
+
+
+@client.command(name='random',
+                description="Returns a random sentence",
+                brief="These sentences may or may not be funny",
+                aliases=["Random", "rand", "rnd", "aléatoire"],
+                pass_context=True)
+async def random_response(context):
+    possible_response = [
+        "Woev!",
+        "Woev, WOEF!!",
+        "woef",
+        "WOEF WOEF ..... WOEF!",
+    ]
+
+    await client.say(random.choice(possible_response) + ', ' + context.message.author.mention)
+
+
+@client.command(name="square")
+async def square(number):
+    squared_value = int(number) * int(number)
+    await client.say(str(number) + " squared is equal to " + str(squared_value))
+
+
+@client.event
+async def on_ready():
+    await(client.change_presence(game=Game(name="Masturbating")))
+    print("Logged in as " + client.user.name)
+
+
+@client.command(name="search")
+async def porn_search(keyword):
+    search_keywords = [keyword]
+    pornhub.client = pornhub.PornHub(search_keywords)
+    for video in pornhub.client.getVideos(5, page=100):
+        await client.say(str(video["name"] + " " + video["url"]))
+
+client.run(config.NjYyNjU5MTUzMDg0NDE2MDMw.Xg9L8w.g4kkUC7o1oBYNGqFwC0Ai4JG-ko)
